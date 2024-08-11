3655227fd0ba2e297481f4c3402c1492f90a244f
Rubén Teijeiro <rubenpol6@gmail.com>
2024-08-11 20:21:19 +0200

    Añadido el ejercicio 105 - Ventas

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Mis soluciones de Acepta el Reto
 102 - [Encriptación De Mensajes](src/aceptaelreto/EncriptacionDeMensajes_102.java)  
 103 - [Problemas De Herencia](src/aceptaelreto/ProblemasDeHerencia_103.java)  
 104 - [Moviles](src/aceptaelreto/Moviles_104.java)  
+105 - [Ventas](src/aceptaelreto/ventas_105.java)  
 106 - [Codigos De Barras](src/aceptaelreto/CodigosDeBarras_106.java)  
 121 - [Chicles de Regalo](src/aceptaelreto/ChiclesDeRegalo_121.java)  
 139 - [Numeros Cubifinitos](src/aceptaelreto/NumerosCubifinitos_139.java)  
---
+++ src/aceptaelreto/Ventas_105.java
@@ -0,0 +1,52 @@
+package aceptaelreto;
+
+import java.io.*;
+
+public class Ventas_105 {
+	public static void main(String[] args) {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String[] nombreDias = { "MARTES", "MIERCOLES", "JUEVES", "VIERNES", "SABADO", "DOMINGO" };
+		StringBuilder outputBuilder = new StringBuilder();
+
+		try {
+			String line;
+			while ((line = reader.readLine().trim()) != null) {
+				double[] valores = new double[6];
+				double valor = Double.parseDouble(line);
+
+				if (valor == -1) {
+					break;
+				}
+
+				valores[0] = valor;
+				double maxVal = valor;
+				double minVal = valor;
+				int diaMax = 0, diaMin = 0;
+				double suma = valor;
+
+				for (int i = 1; i < 6; ++i) {
+					valor = Double.parseDouble(reader.readLine().trim());
+					valores[i] = valor;
+					suma += valor;
+
+					if (valor > maxVal) {
+						maxVal = valor;
+						diaMax = i;
+					}
+					if (valor < minVal) {
+						minVal = valor;
+						diaMin = i;
+					}
+				}
+
+				double media = suma / 6;
+
+				outputBuilder.append(nombreDias[diaMax]).append(' ').append(nombreDias[diaMin]).append(' ')
+						.append(valores[5] > media ? "SI" : "NO").append('\n');
+			}
+			System.out.print(outputBuilder.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
