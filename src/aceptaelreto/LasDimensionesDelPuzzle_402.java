a9fc4e0d59a676497ca6c53ab5da8858d7fd9d10
Rubén Teijeiro <rubenpol6@gmail.com>
2024-08-29 15:08:40 +0200

    Update ejercicio 402 - LasDimensionesDelPuzzle

--- src/aceptaelreto/LasDimensionesDelPuzzle_402.java
+++ src/aceptaelreto/LasDimensionesDelPuzzle_402.java
@@ -1,15 +1,40 @@
 
 package aceptaelreto;
 
-import java.util.Scanner;
+import java.io.*;
 
 public class LasDimensionesDelPuzzle_402 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) {
+		while (true) {
+			String line = reader.readLine();
+			int n = Integer.parseInt(line.trim());
 
-		Scanner sc = new Scanner(System.in);
-		int piezas;
+			if (n == 0) {
+				break;
+			}
 
+			int bestSide = 0;
+			int minDiff = Integer.MAX_VALUE;
+
+			for (int i = 1; i * i <= n; i++) {
+				if (n % i == 0) {
+					int j = n / i;
+					int maxSide = Math.max(i, j);
+					int diff = Math.abs(i - j);
+
+					if (diff < minDiff || (diff == minDiff && maxSide < bestSide)) {
+						bestSide = maxSide;
+						minDiff = diff;
+					}
+				}
+			}
+
+			writer.write(bestSide + "\n");
+		}
+
+		writer.flush();
 	}
-
 }
