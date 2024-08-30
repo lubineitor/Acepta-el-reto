38d7983464a2b5ab6121f67a6f03720bddd7f4a5
Rubén Teijeiro <rubenpol6@gmail.com>
2024-08-30 20:54:28 +0200

    Update ejercicio 169 - CarreraPopular

--- src/aceptaelreto/CarreraPopular_169.java
+++ src/aceptaelreto/CarreraPopular_169.java
@@ -1,17 +1,17 @@
 package aceptaelreto;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class CarreraPopular_169 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int casos = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter pw = new PrintWriter(System.out);
+		int casos = Integer.parseInt(br.readLine());
+		HashMap<String, Integer> map = new HashMap<>();
 		String nombre;
 		String apellido;
-		HashMap<String, Integer> map = new HashMap();
 		int contadorCorredores;
 		int contadorHermanos;
 
@@ -19,24 +19,28 @@ public class CarreraPopular_169 {
 			map.clear();
 			contadorCorredores = 0;
 			contadorHermanos = 0;
-			while (!(nombre = sc.nextLine()).equals("====")) {
+
+			while (!(nombre = br.readLine()).equals("====")) {
 				contadorCorredores++;
 				apellido = nombre.substring(0, nombre.indexOf(",")).toLowerCase();
+
 				if (map.containsKey(apellido)) {
 					map.put(apellido, map.get(apellido) + 1);
 				} else {
 					map.put(apellido, 1);
 				}
 			}
-			for (Map.Entry e : map.entrySet()) {
-				if ((int) e.getValue() > 1) {
-					contadorHermanos += (int) e.getValue();
+
+			for (int count : map.values()) {
+				if (count > 1) {
+					contadorHermanos += count;
 				}
 			}
-			System.out.println(contadorCorredores + " " + contadorHermanos);
 
+			pw.println(contadorCorredores + " " + contadorHermanos);
 		}
 
+		pw.flush();
+		pw.close();
 	}
-
 }
