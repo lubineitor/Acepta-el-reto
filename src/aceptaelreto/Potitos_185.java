bb0233714ec70e8763e462978f01792a8b78da45
Rubén Teijeiro <rubenpol6@gmail.com>
2024-08-31 16:21:58 +0200

    Update ejercicio 185 - Potitos

--- src/aceptaelreto/Potitos_185.java
+++ src/aceptaelreto/Potitos_185.java
@@ -1,50 +1,52 @@
 package aceptaelreto;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Potitos_185 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int casos;
-		ArrayList<String> gustan = new ArrayList();
-		ArrayList<String> noGustan = new ArrayList();
-		String ingrediente;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter pw = new PrintWriter(System.out);
+		String line;
 
-		while ((casos = sc.nextInt()) != 0) {
-			gustan.clear();
-			noGustan.clear();
+		while (!(line = br.readLine()).equals("0")) {
+			int casos = Integer.parseInt(line);
 
-			for (; casos-- > 0;) {
-				if (sc.next().equals("SI:")) {
-					while (!(ingrediente = sc.next()).equals("FIN")) {
-						gustan.add(ingrediente);
-					}
-				} else {
-					while (!(ingrediente = sc.next()).equals("FIN")) {
-						noGustan.add(ingrediente);
+			Set<String> gustan = new HashSet<>();
+			Set<String> noGustan = new TreeSet<>();
+
+			for (int i = 0; i < casos; i++) {
+				String[] parts = br.readLine().split(" ");
+				boolean like = parts[0].equals("SI:");
+
+				for (int j = 1; j < parts.length; j++) {
+					if (parts[j].equals("FIN"))
+						break;
+					if (like) {
+						gustan.add(parts[j]);
+					} else if (!gustan.contains(parts[j])) {
+						noGustan.add(parts[j]);
 					}
 				}
 			}
-//            for (int i = 0; i < gustan.size(); i++) {
-//                noGustan.remove(gustan.get(i));
-//            }
-//            for (String i : gustan) {
-//                noGustan.remove(i);
-//            }
+
 			noGustan.removeAll(gustan);
 
 			if (!noGustan.isEmpty()) {
-				Collections.sort(noGustan);
-				System.out.print(noGustan.get(0));
-				for (int i = 1; i < noGustan.size(); i++) {
-					System.out.print(" " + noGustan.get(i));
+				StringBuilder sb = new StringBuilder();
+				for (String item : noGustan) {
+					if (sb.length() > 0) {
+						sb.append(" ");
+					}
+					sb.append(item);
 				}
-				System.out.println("");
+				pw.println(sb.toString());
+			} else {
+				pw.println();
 			}
 		}
+
+		pw.flush();
 	}
 }
