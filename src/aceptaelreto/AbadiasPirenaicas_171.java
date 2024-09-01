53fe71e24aae1c33b8f90e4cc266413441e82aef
Rubén Teijeiro <rubenpol6@gmail.com>
2024-09-02 00:29:42 +0200

    Update ejercicio 171 - AbadiasPirenaicas

--- src/aceptaelreto/AbadiasPirenaicas_171.java
+++ src/aceptaelreto/AbadiasPirenaicas_171.java
@@ -1,30 +1,42 @@
 package aceptaelreto;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class AbadiasPirenaicas_171 {
-
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int picos;
-		int[] alturas;
-		int maximo;
-		int contador = 0;
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out))) {
 
-		while ((picos = sc.nextInt()) != 0) {
-			alturas = new int[picos];
-			for (int i = 0; i < picos; i++) {
-				alturas[i] = sc.nextInt();
-				maximo = alturas[picos - 1];
-				contador = 1;
-				for (int j = picos - 2; j > -1; j--) {
-					if (alturas[j] > maximo) {
-						maximo = alturas[j];
-						contador++;
-					}
+			String line;
+			while ((line = br.readLine()) != null) {
+				StringTokenizer st = new StringTokenizer(line);
+				int casos = Integer.parseInt(st.nextToken());
+
+				if (casos == 0) {
+					break;
 				}
+
+				ArrayList<Integer> montanas = new ArrayList<>();
+
+				for (int i = 0; i < casos; i++) {
+					int abadia = Integer.parseInt(st.nextToken());
+					boolean valido = true;
+
+					for (int j = montanas.size() - 1; j >= 0 && valido; j--) {
+						if (montanas.get(j) <= abadia) {
+							montanas.remove(j);
+						} else {
+							valido = false;
+						}
+					}
+					montanas.add(abadia);
+				}
+				pw.println(montanas.size());
 			}
-			System.out.println(contador);
+			pw.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
