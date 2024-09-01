a86fe298ce081d6d80438eed3362ea5e4cccc390
Rubén Teijeiro <rubenpol6@gmail.com>
2024-09-01 03:34:58 +0200

    Update ejercicio 176 - CampoDeMinas

--- src/aceptaelreto/CampoDeMinas_176.java
+++ src/aceptaelreto/CampoDeMinas_176.java
@@ -1,64 +1,70 @@
 package aceptaelreto;
 
-import java.util.Scanner;
+import java.io.*;
 
 public class CampoDeMinas_176 {
 
-	private static boolean tieneMinas(char[][] campo, int fila, int col) {
-		int contador = 0;
-		int[] f = { -1, -1, -1, 0, 0, 1, 1, 1 };
-		int[] c = { -1, 0, 1, -1, 1, -1, 0, 1 };
-
-//        if (campo[fila-1][col-1] == '*')  contador++;    
-//        if (campo[fila-1][col] == '*')  contador++;    
-//        if (campo[fila-1][col+1] == '*')  contador++;    
-//        if (campo[fila][col-1] == '*')  contador++;    
-//        if (campo[fila][col+1] == '*')  contador++;    
-//        if (campo[fila+1][col-1] == '*')  contador++;    
-//        if (campo[fila+1][col] == '*')  contador++;    
-//        if (campo[fila+1][col+1] == '*')  contador++; 
-
-		for (int i = 0; i < f.length; i++) {
-			if (campo[fila + f[i]][col + c[i]] == '*') {
-				contador++;
+	public static int buscarMinas(char[][] m, int i, int j) {
+		int[] posF = { 1, -1, 0, 0, 1, -1, -1, 1 };
+		int[] posC = { 0, 0, 1, -1, 1, -1, 1, -1 };
+		int f, c, minas = 0;
+		for (int k = 0; k < 8; k++) {
+			f = i + posF[k];
+			c = j + posC[k];
+			if (safe(f, c) && m[f][c] == '*') {
+				minas++;
 			}
 		}
-		return contador >= 6;
-
+		return minas;
 	}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static boolean safe(int i, int j) {
+		return i >= 0 && i < r && j >= 0 && j < c;
+	}
 
-		int filas, col;
-		char[][] campo;
+	public static int r;
+	public static int c;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter pw = new PrintWriter(System.out);
+		StringBuilder sb = new StringBuilder();
+
+		char[][] m;
 		String linea;
-		int contadorMinas;
 
-		while ((col = sc.nextInt()) != 0) {
-			filas = sc.nextInt();
-			sc.nextLine(); // Vacío el enter del buffer
-			campo = new char[filas][col];
-			for (int fila = 0; fila < campo.length; fila++) {
-				// Con el scanner no puedo leer de carácter en carácter
-				// por lo que leemos una fila completa con el método nextLine()
-				linea = sc.nextLine();
-				for (int columna = 0; columna < campo[0].length; columna++) {
-					campo[fila][columna] = linea.charAt(columna);
+		while (true) {
+			String[] dims = br.readLine().split(" ");
+			c = Integer.parseInt(dims[0]);
+			r = Integer.parseInt(dims[1]);
+			if (r == 0 || c == 0)
+				break;
+
+			m = new char[r][c];
+			for (int i = 0; i < r; i++) {
+				linea = br.readLine();
+				for (int j = 0; j < c; j++) {
+					m[i][j] = linea.charAt(j);
 				}
 			}
 
-			contadorMinas = 0;
-			for (int fila = 1; fila < campo.length - 1; fila++) {
-				for (int columna = 1; columna < campo[0].length - 1; columna++) {
-					if (campo[fila][columna] == '-' && tieneMinas(campo, fila, col)) {
-						contadorMinas++;
+			int minas = 0;
+			int cantidad;
+
+			for (int i = 0; i < r; i++) {
+				for (int j = 0; j < c; j++) {
+					if (m[i][j] == '-') {
+						cantidad = buscarMinas(m, i, j);
+						if (cantidad >= 6)
+							minas++;
 					}
 				}
 			}
-			System.out.println(contadorMinas);
+
+			sb.append(minas).append("\n");
 		}
 
+		pw.print(sb.toString());
+		pw.flush();
 	}
-
 }
