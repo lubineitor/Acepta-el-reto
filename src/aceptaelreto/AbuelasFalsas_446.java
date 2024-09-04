66348b95034d5460496a874128188da5cb289378
Rubén Teijeiro <rubenpol6@gmail.com>
2024-09-04 02:07:00 +0200

    Update ejercicio 446 - AbuelasFalsas

--- src/aceptaelreto/AbuelasFalsas_446.java
+++ src/aceptaelreto/AbuelasFalsas_446.java
@@ -1,45 +1,43 @@
 package aceptaelreto;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class AbuelasFalsas_446 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter pw = new PrintWriter(System.out);
+		int casos = Integer.parseInt(br.readLine().trim());
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int casos = sc.nextInt();
-		int numNietos;
-		String nieto, ultimoNieto, nietoActual;
-		boolean abuelaFalsa;
+		for (int c = 0; c < casos; c++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String nieto = st.nextToken();
+			int numNietos = Integer.parseInt(st.nextToken());
+			boolean abuelaFalsa = false;
 
-		for (; casos-- > 0;) {
-			nieto = sc.next();
-			numNietos = sc.nextInt();
-			abuelaFalsa = false;
 			if (numNietos == 1) {
-				System.out.println("FALSA");
-				sc.nextLine();
+				pw.println("FALSA");
 			} else {
-				for (int i = 0; i < numNietos - 1; i++) {
-					nietoActual = sc.next();
+				for (int i = 1; i < numNietos; i++) {
+					String nietoActual = st.nextToken();
 					if (nietoActual.equals(nieto)) {
-						System.out.println("FALSA");
-						sc.nextLine();
+						pw.println("FALSA");
 						abuelaFalsa = true;
 						break;
 					}
 				}
 				if (!abuelaFalsa) {
-					ultimoNieto = sc.nextLine();
-					ultimoNieto = ultimoNieto.trim();
+					String ultimoNieto = st.hasMoreTokens() ? st.nextToken() : br.readLine().trim();
 					if (ultimoNieto.equals(nieto)) {
-						System.out.println("VERDADERA");
+						pw.println("VERDADERA");
 					} else {
-						System.out.println("FALSA");
+						pw.println("FALSA");
 					}
 				}
 			}
 		}
 
+		pw.flush();
+		pw.close();
 	}
-
 }
