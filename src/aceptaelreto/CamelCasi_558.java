8e2cc954cda53a5a70a3fa57979f0073bee30d6a
Rubén Teijeiro <rubenpol6@gmail.com>
2024-09-05 07:11:53 +0200

    Update ejercicio 558 - CamelCasi

--- src/aceptaelreto/CamelCasi_558.java
+++ src/aceptaelreto/CamelCasi_558.java
@@ -1,44 +1,49 @@
 package aceptaelreto;
 
-import java.util.HashMap;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class CamelCasi_558 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter pw = new PrintWriter(System.out);
 		String[] claves;
 		int numVariables;
-		HashMap<String, StringBuilder> map = new HashMap();
+		HashMap<String, StringBuilder> map = new HashMap<>();
 		String variable, clave;
 		StringBuilder sb;
 
-		while (sc.hasNext()) {
+		String line;
+		while ((line = br.readLine()) != null && !line.isEmpty()) {
+			numVariables = Integer.parseInt(line.trim());
 			map.clear();
-			numVariables = sc.nextInt();
 			claves = new String[numVariables];
-			sc.nextLine(); // Limpiar el Enter
+
 			for (int i = 0; i < numVariables; i++) {
-				variable = sc.nextLine();
+				variable = br.readLine();
 				clave = variable.toUpperCase();
 				claves[i] = clave;
 				sb = map.get(clave);
+
 				if (sb == null) {
 					map.put(clave, new StringBuilder(variable));
 				} else {
-					for (int j = 0; j < sb.length(); j++) {
-						if (variable.charAt(j) <= 90) { // 90 código ASCII de 'z'
+					for (int j = 0; j < variable.length(); j++) {
+						if (variable.charAt(j) <= 'Z') {
 							sb.setCharAt(j, variable.charAt(j));
 						}
-						map.put(clave, sb);
 					}
+					map.put(clave, sb);
 				}
 			}
-			for (int i = 0; i < claves.length; i++) {
-				System.out.println(map.get(claves[i]));
-			}
-			System.out.println("---");
-		}
-	}
 
+			for (int i = 0; i < claves.length; i++) {
+				pw.println(map.get(claves[i]));
+			}
+			pw.println("---");
+		}
+
+		pw.flush();
+	}
 }
