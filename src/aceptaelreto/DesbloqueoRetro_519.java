42ef5aaee8fd4252e88327f2b8e1c2c55c75819c
Rubén Teijeiro <rubenpol6@gmail.com>
2024-09-15 19:34:21 +0200

    Update ejercicio 519 - DesbloqueoRetro

--- src/aceptaelreto/DesbloqueoRetro_519.java
+++ src/aceptaelreto/DesbloqueoRetro_519.java
@@ -1,153 +1,163 @@
 package aceptaelreto;
 
-import java.util.Scanner;
+import java.io.*;
 
 public class DesbloqueoRetro_519 {
 
-	private static void inicializarPantalla(char[][] pantalla) {
-		// Esquinas
-		pantalla[0][0] = '+';
-		pantalla[0][pantalla[0].length - 1] = '+';
-		pantalla[pantalla.length - 1][0] = '+';
-		pantalla[pantalla.length - 1][pantalla[0].length - 1] = '+';
-
-		// Lineas horizontales arriba y abajo
-		for (int col = 1; col < pantalla[0].length - 1; col++) {
-			pantalla[0][col] = '-';
-			pantalla[pantalla.length - 1][col] = '-';
-		}
-
-		// Lineas verticales izquierda y derecha
-		for (int fil = 1; fil < pantalla.length - 1; fil++) {
-			pantalla[fil][0] = '|';
-			pantalla[fil][pantalla[0].length - 1] = '|';
-		}
-
-		for (int f = 1; f < pantalla.length - 1; f++) {
-			for (int c = 1; c < pantalla[0].length - 1; c++) {
-				pantalla[f][c] = ' ';
-			}
-		}
-
-		for (int f = 1; f < pantalla.length - 1; f += 2) {
-			for (int c = 1; c < pantalla[0].length - 1; c += 2) {
-				pantalla[f][c] = '.';
-			}
-		}
+	public static void ini(String[][] arr) {
+		for (int i = 0; i < arr.length; i++)
+			for (int j = 0; j < arr[0].length; j++)
+				arr[i][j] = "";
 	}
 
-	private static void verPantalla(char[][] pantalla) {
-		for (int f = 0; f < pantalla.length; f++) {
-			for (int c = 0; c < pantalla[0].length; c++) {
-				System.out.print(pantalla[f][c]);
-			}
-			System.out.println("");
+	public static boolean contieneVertical(String[][] arr, int f) {
+		for (int c = 0; c < arr[0].length; c++) {
+			if (arr[f][c].contains("1") || arr[f][c].contains("3") || arr[f][c].contains("2"))
+				return true;
+			if (arr[f + 1][c].contains("8") || arr[f + 1][c].contains("7") || arr[f + 1][c].contains("9"))
+				return true;
 		}
+
+		return false;
 	}
 
-	private static void procesarMovimientos(char[][] pantalla, String movimientos, int fila, int col) {
-		// Se resta 1 a la longitud de los movimientos porque
-		// el último carácter es un 5 y no hay que procesarlo
-		for (int i = 0; i < movimientos.length() - 1; i++) {
-			switch (movimientos.charAt(i)) {
-			case '1':
-				fila--;
-				col--;
-				if (pantalla[fila][col] != ' ') {
-					pantalla[fila][col] = 'X';
-				} else {
-					pantalla[fila][col] = '\\';
-				}
-				fila--;
-				col--;
-				pantalla[fila][col] = 'O';
-				break;
-			case '2':
-				fila--;
-				pantalla[fila][col] = '|';
-				fila--;
-				pantalla[fila][col] = 'O';
-				break;
-			case '3':
-				fila--;
-				col++;
-				if (pantalla[fila][col] != ' ') {
-					pantalla[fila][col] = 'X';
-				} else {
-					pantalla[fila][col] = '/';
-				}
-				fila--;
-				col++;
-				pantalla[fila][col] = 'O';
-				break;
-			case '4':
-				col--;
-				pantalla[fila][col] = '-';
-				col--;
-				pantalla[fila][col] = 'O';
-				break;
-			case '6':
-				col++;
-				pantalla[fila][col] = '-';
-				col++;
-				pantalla[fila][col] = 'O';
-				break;
-			case '7':
-				fila++;
-				col--;
-				if (pantalla[fila][col] != ' ') {
-					pantalla[fila][col] = 'X';
-				} else {
-					pantalla[fila][col] = '/';
-				}
-				fila++;
-				col--;
-				pantalla[fila][col] = 'O';
-				break;
-			case '8':
-				fila++;
-				pantalla[fila][col] = '|';
-				fila++;
-				pantalla[fila][col] = 'O';
-				break;
-			case '9':
-				fila++;
-				col++;
-				if (pantalla[fila][col] != ' ') {
-					pantalla[fila][col] = 'X';
-				} else {
-					pantalla[fila][col] = '\\';
-				}
-				fila++;
-				col++;
-				pantalla[fila][col] = 'O';
-				break;
-			}
-		}
+	public static boolean contieneDiagonalArriba(String[][] arr, int f, int c) {
+		return (c < arr[0].length - 1 && (arr[f + 1][c].contains("7") || arr[f][c + 1].contains("3")));
 	}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int filas, columnas;
-		int filaOrigen, colOrigen;
-		char[][] pantalla;
-		String movimientos;
+	public static boolean contieneDiagonalAbajo(String[][] arr, int f, int c) {
+		return (c < arr[0].length - 1 && (arr[f][c].contains("1") || arr[f + 1][c + 1].contains("9")));
+	}
 
-		while ((filas = sc.nextInt()) != 0) {
-			columnas = sc.nextInt();
-			pantalla = new char[filas + 2 + (filas - 1)][columnas + 2 + (columnas - 1)];
+	public static void main(String[] args) throws IOException {
 
-			inicializarPantalla(pantalla);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter pw = new PrintWriter(System.out);
 
-			filaOrigen = sc.nextInt();
-			colOrigen = sc.nextInt();
-			sc.nextLine(); // Vaciar enter
-			pantalla[filaOrigen][colOrigen] = 'O';
+		int f, c, posF, posC, num, pos;
+		String[][] arr;
+		String numeros;
 
-			movimientos = sc.nextLine();
-			procesarMovimientos(pantalla, movimientos, filaOrigen, colOrigen);
+		while (true) {
 
-			verPantalla(pantalla);
+			String[] dimensions = br.readLine().split(" ");
+			f = Integer.parseInt(dimensions[0]);
+			c = Integer.parseInt(dimensions[1]);
+			if (f == 0 && c == 0)
+				break;
+
+			String[] position = br.readLine().split(" ");
+			posF = Integer.parseInt(position[0]) - 1;
+			posC = Integer.parseInt(position[1]) - 1;
+			arr = new String[f][c];
+			ini(arr);
+			arr[posF][posC] = "O";
+
+			numeros = br.readLine();
+			pos = 0;
+			num = Integer.parseInt("" + numeros.charAt(pos++));
+			while (num != 5) {
+
+				switch (num) {
+				case 1:
+					arr[--posF][--posC] += "1 ";
+					break;
+				case 2:
+					arr[--posF][posC] += "2 ";
+					break;
+				case 3:
+					arr[--posF][++posC] += "3 ";
+					break;
+				case 4:
+					arr[posF][--posC] += "4 ";
+					break;
+				case 6:
+					arr[posF][++posC] += "6 ";
+					break;
+				case 7:
+					arr[++posF][--posC] += "7 ";
+					break;
+				case 8:
+					arr[++posF][posC] += "8 ";
+					break;
+				case 9:
+					arr[++posF][++posC] += "9 ";
+				}
+
+				num = Integer.parseInt("" + numeros.charAt(pos++));
+			}
+
+			for (int i = 0; i < f; i++) {
+
+				if (i == 0) {
+					for (int j = 0; j < c * 2 + 1; j++) {
+						if (j == 0 || j == c * 2)
+							pw.print("+");
+						else
+							pw.print("-");
+					}
+					pw.println();
+				}
+
+				for (int j = 0; j < c; j++) {
+					if (j == 0)
+						pw.print("|");
+					pw.print((arr[i][j].isEmpty()) ? "." : "O");
+					if (arr[i][j].contains("4") || (j + 1 < c && arr[i][j + 1].contains("6")))
+						pw.print("-");
+					else if (j < c - 1)
+						pw.print(" ");
+					if (j == c - 1)
+						pw.print("|");
+				}
+
+				if (i < f - 1 && contieneVertical(arr, i)) {
+					pw.println();
+					for (int j = 0; j < c; j++) {
+						if (j == 0)
+							pw.print("|");
+						if (arr[i + 1][j].contains("8") || arr[i][j].contains("2"))
+							pw.print("|");
+						else
+							pw.print(" ");
+						if (j < c - 1) {
+							if (contieneDiagonalArriba(arr, i, j) && contieneDiagonalAbajo(arr, i, j))
+								pw.print("X");
+							else if (contieneDiagonalAbajo(arr, i, j))
+								pw.print("\\");
+							else if (contieneDiagonalArriba(arr, i, j))
+								pw.print("/");
+							else
+								pw.print(" ");
+						}
+						if (j == c - 1)
+							pw.print("|");
+					}
+				} else if (i < f - 1) {
+					for (int j = 0; j < c + (c - 1); j++) {
+						if (j == 0)
+							pw.print("\n|");
+						pw.print(" ");
+						if (j == (c + c - 2))
+							pw.print("|");
+					}
+				}
+
+				if (i == f - 1) {
+					pw.println();
+					for (int j = 0; j < c * 2 + 1; j++) {
+						if (j == 0 || j == c * 2)
+							pw.print("+");
+						else
+							pw.print("-");
+					}
+					pw.println();
+				} else
+					pw.println();
+			}
+
 		}
+
+		pw.flush();
 	}
 }
