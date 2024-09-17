e5f1d213cb21e21a680d3adee2239b6ea9663737
Rubén Teijeiro <rubenpol6@gmail.com>
2024-09-17 22:59:25 +0200

    Update ejercicio 130 - MejorImposible

--- src/aceptaelreto/MejorImposible_130.java
+++ src/aceptaelreto/MejorImposible_130.java
@@ -4,37 +4,39 @@ import java.io.*;
 
 public class MejorImposible_130 {
 
-	public static void main(String[] args) {
-		StringBuilder output = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter writer = new PrintWriter(new BufferedOutputStream(System.out));
 
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-			String line;
-			while ((line = reader.readLine()) != null) {
-				int N = Integer.parseInt(line);
-				if (N == 0) {
-					break;
-				}
+		String line = reader.readLine();
+		int size = Integer.parseInt(line);
 
-				String[] coordinates = reader.readLine().split(" ");
-				int x1 = Integer.parseInt(coordinates[0]);
-				int y1 = Integer.parseInt(coordinates[1]);
-				int x2 = Integer.parseInt(coordinates[2]);
-				int y2 = Integer.parseInt(coordinates[3]);
+		while (size != 0) {
+			line = reader.readLine();
+			String[] parts = line.split(" ");
+			long startX = Long.parseLong(parts[0]);
+			long startY = Long.parseLong(parts[1]);
+			long endX = Long.parseLong(parts[2]);
+			long endY = Long.parseLong(parts[3]);
 
-				if ((x1 + y1) % 2 != (x2 + y2) % 2) {
-					output.append("IMPOSIBLE\n");
-				} else {
-					if (x1 == x2 || y1 == y2 || Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
-						output.append("1\n");
-					} else {
-						output.append("2\n");
-					}
-				}
+			if (((startX + startY) % 2) != ((endX + endY) % 2)) {
+				writer.println("IMPOSIBLE");
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
+			else if (startX == endX && startY == endY) {
+				writer.println("0");
+			}
+			else if ((startX + startY == endX + endY) || (startX - startY == endX - endY)) {
+				writer.println("1");
+			}
+			else {
+				writer.println("2");
+			}
+
+			line = reader.readLine();
+			size = Integer.parseInt(line);
 		}
 
-		System.out.print(output.toString());
+		reader.close();
+		writer.close();
 	}
 }
