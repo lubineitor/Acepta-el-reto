d3890ee9aa8f8ff0ca280e3e4a71738fd3e1c3e2
Rubén Teijeiro <rubenpol6@gmail.com>
2024-09-24 17:41:36 +0200

    Update ejercicio 593 - PruebasPorLotes

--- src/aceptaelreto/PruebasPorLotes_593.java
+++ src/aceptaelreto/PruebasPorLotes_593.java
@@ -4,36 +4,34 @@ import java.io.*;
 
 public class PruebasPorLotes_593 {
 
-    private static boolean hayPositivo(String muestras, int desde, int hasta) {
-        for (int i = desde; i < hasta; i++) {
-            if (muestras.charAt(i) == '1') {
-                return true;
-            }
-        }
-        return false;
-    }
+	private static boolean hayPositivo(String muestras, int desde, int hasta) {
+		for (int i = desde; i < hasta; i++) {
+			if (muestras.charAt(i) == '1') {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    private static int numeroPruebas(String muestras, int desde, int hasta) {
-        if (desde == hasta - 1 || !hayPositivo(muestras, desde, hasta)) {
-            return 1;
-        } else {
-            int suma = desde + hasta;
-            if ((hasta - desde) % 2 == 0) {
-                return 1 + numeroPruebas(muestras, desde, suma / 2) + numeroPruebas(muestras, suma / 2, hasta);
-            } else {
-                return 1 + numeroPruebas(muestras, desde, suma / 2 + 1) + numeroPruebas(muestras, suma / 2 + 1, hasta);
-            }
-        }
-    }
+	private static int numeroPruebas(String muestras, int desde, int hasta) {
+		if (desde == hasta - 1 || !hayPositivo(muestras, desde, hasta)) {
+			return 1;
+		} else {
+			int mitad = (desde + hasta) / 2;
+			return 1 + numeroPruebas(muestras, desde, mitad) + numeroPruebas(muestras, mitad, hasta);
+		}
+	}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out, true);
-        
-        String line;
-        while (!(line = in.readLine()).equals("0")) {
-            String muestras = in.readLine();
-            out.println(numeroPruebas(muestras, 0, muestras.length()));
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+
+		String line;
+		while (!(line = in.readLine()).equals("0")) {
+			String muestras = in.readLine();
+			out.println(numeroPruebas(muestras, 0, muestras.length()));
+		}
+
+		out.flush();
+	}
 }
