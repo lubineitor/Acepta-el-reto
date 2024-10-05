3eebcc07eac87faafea64a2fa9cca21451027cce
Rubén Teijeiro <rubenpol6@gmail.com>
2024-10-05 08:52:12 +0200

    Update ejercicio 663 - ContandoDesdeElCero

--- src/aceptaelreto/ContandoDesdeElCero_663.java
+++ src/aceptaelreto/ContandoDesdeElCero_663.java
@@ -5,25 +5,23 @@ import java.io.*;
 public class ContandoDesdeElCero_663 {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter pw = new PrintWriter(System.out);
-        StringBuilder sb = new StringBuilder();
-        
-        int casos = Integer.parseInt(br.readLine());
-        
-        for (int i = 0; i < casos; i++) {
-            int anio = Integer.parseInt(br.readLine());
-            
-            if (anio > 0) {
-                sb.append(anio - 1).append("\n");
-            } else {
-                sb.append(anio).append("\n");
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+            int casos = Integer.parseInt(br.readLine());
+
+            for (int i = 0; i < casos; i++) {
+                int anio = Integer.parseInt(br.readLine());
+                
+                if (anio > 0) {
+                    bw.write(String.valueOf(anio - 1));
+                } else {
+                    bw.write(String.valueOf(anio));
+                }
+                bw.newLine();
             }
+
+            bw.flush();
         }
-        
-        pw.print(sb.toString());
-        pw.flush();
-        pw.close();
     }
 }
-
