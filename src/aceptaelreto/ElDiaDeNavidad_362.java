5a728efb3964b337de421cba7982879a3a88522c
Rubén Teijeiro <rubenpol6@gmail.com>
2024-10-06 15:12:21 +0200

    Update ejercicio 362 - ElDiaDeNavidad

--- src/aceptaelreto/ElDiaDeNavidad_362.java
+++ src/aceptaelreto/ElDiaDeNavidad_362.java
@@ -4,28 +4,24 @@ import java.io.*;
 
 public class ElDiaDeNavidad_362 {
 	public static void main(String[] args) throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		PrintWriter writer = new PrintWriter(System.out);
-        StringBuilder output = new StringBuilder();
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+			StringBuilder output = new StringBuilder();
 
+			final int N = Integer.parseInt(reader.readLine());
 
-		int N = Integer.parseInt(reader.readLine());
+			for (int i = 0; i < N; i++) {
+				String[] fecha = reader.readLine().split(" ");
+				final int dia = Integer.parseInt(fecha[0]);
+				final int mes = Integer.parseInt(fecha[1]);
 
-		for (int i = 0; i < N; i++) {
-			String[] fecha = reader.readLine().split(" ");
-			int dia = Integer.parseInt(fecha[0]);
-			int mes = Integer.parseInt(fecha[1]);
+				if (dia == 25 && mes == 12) {
+					output.append("SI\n");
+				} else {
+					output.append("NO\n");
+				}
+			}
 
-			if (dia == 25 && mes == 12) {
-                output.append("SI\n");
-            } else {
-                output.append("NO\n");
-            }
+			System.out.print(output);
 		}
-
-		writer.print(output);
-		writer.flush();
-		writer.close();
-		reader.close();
 	}
 }
