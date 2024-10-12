8328dac2bf809df4389e89b6f097b473f152d006
Rubén Teijeiro <rubenpol6@gmail.com>
2024-10-12 22:10:16 +0200

    Update ejercicio 667 - Pipos

--- src/aceptaelreto/Pipos_667.java
+++ src/aceptaelreto/Pipos_667.java
@@ -1,37 +1,45 @@
 package aceptaelreto;
 
-import java.util.Scanner;
+import java.io.*;
 
 public class Pipos_667 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				PrintWriter pw = new PrintWriter(System.out)) {
+			while (true) {
+				String year = br.readLine();
 
-		while (true) {
-			String year = sc.nextLine();
-			if (year.equals("0")) {
-				break;
-			}
-			int[] digits = new int[10];
-			for (int i = 0; i < 10; i++) {
-				digits[i] = sc.nextInt();
-			}
-			sc.nextLine();
-			int[] counts = new int[10];
-			for (char c : year.toCharArray()) {
-				if (Character.isDigit(c)) {
-					counts[c - '0']++;
+				if (year.equals("0")) {
+					break;
 				}
-			}
-			int minValue = Integer.MAX_VALUE;
-			for (int i = 0; i < 10; i++) {
-				if (counts[i] > 0) {
-					digits[i] /= counts[i];
-					minValue = Math.min(minValue, digits[i]);
+
+				String[] input = br.readLine().split(" ");
+				int[] stock = new int[10];
+				for (int i = 0; i < 10; i++) {
+					stock[i] = Integer.parseInt(input[i]);
 				}
+
+				int[] digitCount = new int[10];
+				for (char c : year.toCharArray()) {
+					if (Character.isDigit(c)) {
+						digitCount[c - '0']++;
+					}
+				}
+
+				int minPipos = Integer.MAX_VALUE;
+
+				for (int i = 0; i < 10; i++) {
+					if (digitCount[i] > 0) {
+						int piposPorDigito = stock[i] / digitCount[i];
+						minPipos = Math.min(minPipos, piposPorDigito);
+					}
+				}
+
+				pw.println(minPipos == Integer.MAX_VALUE ? 0 : minPipos / 3);
 			}
-			System.out.println(minValue == Integer.MAX_VALUE ? 0 : minValue / 3);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		sc.close();
 	}
 }
