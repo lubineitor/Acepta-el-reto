b6ba6eea070245efc5faa84bab3b0e5e9d772bf1
Rubén Teijeiro <rubenpol6@gmail.com>
2024-10-26 12:46:24 +0200

    Update ejercicio 270 - MePilloElToro

--- src/aceptaelreto/MePilloElToro_270.java
+++ src/aceptaelreto/MePilloElToro_270.java
@@ -1,44 +1,37 @@
 package aceptaelreto;
 
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.*;
 
 public class MePilloElToro_270 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int casos;
-		String nombre, correcion;
-		TreeMap<String, Integer> map = new TreeMap();
+		TreeMap<String, Integer> map = new TreeMap<>();
 
-		while ((casos = sc.nextInt()) != 0) {
+		while (true) {
+			int casos = sc.nextInt();
+			if (casos == 0)
+				break;
+
 			sc.nextLine();
 			map.clear();
-			while (casos-- > 0) {
-				nombre = sc.nextLine();
-				correcion = sc.nextLine();
-				if (map.containsKey(nombre)) {
-					if (correcion.equals("CORRECTO")) {
-						map.put(nombre, map.get(nombre) + 1);
-					} else {
-						map.put(nombre, map.get(nombre) - 1);
-					}
-				} else {
-					if (correcion.equals("CORRECTO")) {
-						map.put(nombre, 1);
-					} else {
-						map.put(nombre, -1);
-					}
-				}
+
+			for (int i = 0; i < casos; i++) {
+				String nombre = sc.nextLine();
+				String correccion = sc.nextLine();
+
+				int valor = correccion.equals("CORRECTO") ? 1 : -1;
+				map.put(nombre, map.getOrDefault(nombre, 0) + valor);
 			}
-		}
-		for (Map.Entry entry : map.entrySet()) {
-			if ((int) entry.getValue() != 0) {
-				System.out.println(entry.getKey() + ", " + entry.getValue());
+
+			for (Map.Entry<String, Integer> entry : map.entrySet()) {
+				if (entry.getValue() != 0) {
+					System.out.println(entry.getKey() + ", " + entry.getValue());
+				}
 			}
 			System.out.println("---");
 		}
-	}
 
+		sc.close();
+	}
 }
