a794dc9425fc7a89fac53e351476d8813c9931e9
Rubén Teijeiro <rubenpol6@gmail.com>
2024-11-04 20:48:41 +0100

    Update ejercicio 139 - NumerosCubifinitos

--- src/aceptaelreto/NumerosCubifinitos_139.java
+++ src/aceptaelreto/NumerosCubifinitos_139.java
@@ -1,40 +1,51 @@
 package aceptaelreto;
 
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class NumerosCubifinitos_139 {
 
-	private final static HashSet<Integer> numeros = new HashSet();
+	private final static HashSet<Integer> numeros = new HashSet<>();
 
-	private static void esCubifinito(int n) {
+	private static void esCubifinito(int n, PrintWriter out) {
 		if (n == 1) {
-			System.out.println(" -> cubifinito.");
+			out.println(" -> cubifinito.");
 		} else {
 			int nuevo = 0;
-			while (n != 0) {
-				nuevo += Math.pow(n % 10, 3);
-				n /= 10;
+			int temp = n;
+			while (temp != 0) {
+				nuevo += Math.pow(temp % 10, 3);
+				temp /= 10;
 			}
 			if (numeros.contains(nuevo)) {
-				System.out.println(" - " + nuevo + " -> no cubifinito.");
+				out.println(" - " + nuevo + " -> no cubifinito.");
 			} else {
 				numeros.add(nuevo);
-				System.out.print(" - " + nuevo);
-				esCubifinito(nuevo);
+				out.print(" - " + nuevo);
+				esCubifinito(nuevo, out);
 			}
 		}
 	}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n;
+	public static void main(String[] args) throws IOException {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				PrintWriter out = new PrintWriter(System.out, true)) {
 
-		while ((n = sc.nextInt()) != 0) {
-			numeros.clear();
-			System.out.print(n);
-			numeros.add(n);
-			esCubifinito(n);
+			int n;
+			while (true) {
+				n = Integer.parseInt(br.readLine().trim());
+				if (n == 0)
+					break;
+
+				numeros.clear();
+				out.print(n);
+				numeros.add(n);
+				esCubifinito(n, out);
+			}
+
+			out.flush();
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
 		}
 	}
 }
