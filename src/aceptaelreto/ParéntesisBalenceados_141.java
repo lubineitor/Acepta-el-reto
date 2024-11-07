da24a57b545db3c50b8b5a59a328e1f7d51bacbe
Rubén Teijeiro <113635589+lubineitor@users.noreply.github.com>
2024-11-07 08:10:12 +0100

    Update ejercicio 141 -  ParéntesisBalenceados

--- src/aceptaelreto/ParéntesisBalenceados_141.java
+++ src/aceptaelreto/ParéntesisBalenceados_141.java
@@ -1,39 +1,52 @@
 package aceptaelreto;
 
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
-public class ParéntesisBalenceados_141 {
+public class ParentesisBalanceados_141 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String expresion;
-		char caracter, caracterRecuperado;
-		Stack<Character> pila = new Stack();
-		boolean balanceados;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        String expresion;
+        Stack<Character> pila = new Stack<>();
 
-		while (sc.hasNext()) {
-			expresion = sc.nextLine();
-			balanceados = true;
-			pila.clear();
+        while ((expresion = br.readLine()) != null) {
+            boolean balanceados = true;
+            pila.clear();
 
-			for (int i = 0; i < expresion.length(); i++) {
-				caracter = expresion.charAt(i);
+            for (int i = 0; i < expresion.length(); i++) {
+                char caracter = expresion.charAt(i);
 
-				if (caracter == '(' || caracter == '{' || caracter == '[') {
-					pila.push(caracter);
-				} else if (caracter == ')' || caracter == '}' || caracter == ']') {
-					if (pila.isEmpty()) {
-						balanceados = false;
-						break;
-					} else {
-						caracterRecuperado = pila.pop();
+                if (caracter == '(' || caracter == '{' || caracter == '[') {
+                    pila.push(caracter);
+                } else if (caracter == ')' || caracter == '}' || caracter == ']') {
+                    if (pila.isEmpty()) {
+                        balanceados = false;
+                        break;
+                    } else {
+                        char caracterRecuperado = pila.pop();
+                        if (!esParBalanceado(caracterRecuperado, caracter)) {
+                            balanceados = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            
+            if (balanceados && pila.isEmpty()) {
+                pw.println("YES");
+            } else {
+                pw.println("NO");
+            }
+        }
+        
+        pw.flush();
+    }
 
-					}
-				}
-			}
-		}
-
-	}
-
+    private static boolean esParBalanceado(char apertura, char cierre) {
+        return (apertura == '(' && cierre == ')') ||
+               (apertura == '{' && cierre == '}') ||
+               (apertura == '[' && cierre == ']');
+    }
 }
