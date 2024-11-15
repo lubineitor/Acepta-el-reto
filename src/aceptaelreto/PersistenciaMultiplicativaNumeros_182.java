7d0362277079ce4f82e5821ceb6ee0f578776a2e
Rubén Teijeiro <113635589+lubineitor@users.noreply.github.com>
2024-11-15 20:36:18 +0100

    Update ejercicio 182 - PersistenciaMultiplicativaNumeros

--- src/aceptaelreto/PersistenciaMultiplicativaNumeros_182.java
+++ src/aceptaelreto/PersistenciaMultiplicativaNumeros_182.java
@@ -1,38 +1,39 @@
 package aceptaelreto;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class PersistenciaMultiplicativaNumeros_182 {
 
-	private static int longitudNumero(int num) {
-		return 0;
+    private static int longitudNumero(int num) {
+        return String.valueOf(num).length();
+    }
 
-	}
+    private static int persistencia(int num) {
+        int persistencia = 0;
 
-	private static int persistencia(int num) {
-		int persistencia, multiplicacion, longitud;
-		persistencia = 0;
+        while (num >= 10) {
+            persistencia++;
+            int multiplicacion = 1;
 
-		while ((longitud = longitudNumero(num)) != 1) {
-			persistencia++;
-			multiplicacion = 1;
-			for (int i = 0; i < longitud; i++) {
-				multiplicacion *= num % 10;
-				num /= 10;
-			}
-			num = multiplicacion;
-		}
-		return persistencia;
-	}
+            while (num > 0) {
+                multiplicacion *= num % 10;
+                num /= 10;
+            }
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int casos = sc.nextInt();
+            num = multiplicacion;
+        }
+        return persistencia;
+    }
 
-		for (; casos > 0; casos--) {
-			System.out.println(persistencia(sc.nextInt()));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int casos = sc.nextInt();
 
-		}
-	}
+        for (int i = 0; i < casos; i++) {
+            int num = sc.nextInt();
+            System.out.println(persistencia(num));
+        }
 
+        sc.close();
+    }
 }
