1fd1ec928708e855a0bc010124c4d8bf13e8d245
Rubén Teijeiro <rubenpol6@gmail.com>
2024-12-08 20:18:31 +0100

    Update ejercicio 344 - ConectandoCables

--- src/aceptaelreto/ConectandoCables_344.java
+++ src/aceptaelreto/ConectandoCables_344.java
@@ -3,33 +3,39 @@ package aceptaelreto;
 import java.io.*;
 
 public class ConectandoCables_344 {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-		int casosPrueba = Integer.parseInt(reader.readLine().trim());
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             PrintWriter writer = new PrintWriter(System.out)) {
 
-		for (int i = 0; i < casosPrueba; i++) {
-			reader.readLine();
-			String cables = reader.readLine().trim();
+            int casosPrueba = Integer.parseInt(reader.readLine().trim());
 
-			int macho = 0;
-			int hembra = 0;
+            for (int i = 0; i < casosPrueba; i++) {
+                reader.readLine();
+                String cables = reader.readLine().trim();
 
-			for (int j = 0; j < cables.length(); j++) {
-				char conector = cables.charAt(j);
-				if (conector == 'M') {
-					macho++;
-				} else if (conector == 'H') {
-					hembra++;
-				}
-			}
+                int macho = 0;
+                int hembra = 0;
 
-			if (macho == hembra) {
-				System.out.println("POSIBLE");
-			} else {
-				System.out.println("IMPOSIBLE");
-			}
-		}
-	}
+                for (int j = 0; j < cables.length(); j++) {
+                    char conector = cables.charAt(j);
+                    if (conector == 'M') {
+                        macho++;
+                    } else if (conector == 'H') {
+                        hembra++;
+                    }
+                }
+
+                if (macho == hembra) {
+                    writer.println("POSIBLE");
+                } else {
+                    writer.println("IMPOSIBLE");
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } catch (NumberFormatException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
