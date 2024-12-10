8a277087345cda3633b8c64bb86134de8cf2ac0a
Rubén Teijeiro <rubenpol6@gmail.com>
2024-12-10 15:10:50 +0100

    Update ejercicio 119 - EscudosDelEjercitoRomano

--- src/aceptaelreto/EscudosDelEjercitoRomano_119.java
+++ src/aceptaelreto/EscudosDelEjercitoRomano_119.java
@@ -3,31 +3,42 @@ package aceptaelreto;
 import java.io.*;
 
 public class EscudosDelEjercitoRomano_119 {
-	
-	public static void main(String[] args) throws IOException {
 
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		String inputLine;
+	public static void main(String[] args) {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+			String inputLine;
 
-		while ((inputLine = reader.readLine()) != null) {
-			int ejercito = Integer.parseInt(inputLine);
-			if (ejercito == 0)
-				break;
-
-			int total = 0;
-			while (ejercito > 0) {
-				if (ejercito < 4) {
-					total += ejercito * 5;
+			while ((inputLine = reader.readLine()) != null) {
+				int ejercito = Integer.parseInt(inputLine);
+				if (ejercito == 0) {
 					break;
 				}
 
-				int temp = (int) Math.sqrt(ejercito);
-				ejercito -= temp * temp;
-
-				total += (temp - 2) * (temp - 2);
-				total += ((temp - 2) * 4) * 2 + 12;
+				int totalEscudos = calcularEscudos(ejercito);
+				System.out.println(totalEscudos);
 			}
-			System.out.println(total);
+		} catch (IOException e) {
+			System.err.println("Error leyendo la entrada: " + e.getMessage());
 		}
+	}
+
+	private static int calcularEscudos(int ejercito) {
+		int total = 0;
+
+		while (ejercito > 0) {
+			if (ejercito < 4) {
+				total += ejercito * 5;
+				break;
+			}
+
+			int ladoCuadrado = (int) Math.sqrt(ejercito);
+			ejercito -= ladoCuadrado * ladoCuadrado;
+
+			int centro = (ladoCuadrado - 2) * (ladoCuadrado - 2);
+			int borde = ((ladoCuadrado - 2) * 4) * 2 + 12;
+			total += centro + borde;
+		}
+
+		return total;
 	}
 }
